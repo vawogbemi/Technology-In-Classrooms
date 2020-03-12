808dabbf862f47fe21731727a4d45297cae49e63
victorawogbemi <vawogbemi@gmail.com>
2020-03-12 15:48:34 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "intersection-observer";
+import scrollama from "scrollama";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
